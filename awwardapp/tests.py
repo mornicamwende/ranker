ded040f85959ddd38d372336f9371dc3103813df
Mornica Mwende <mornicamwende@gmail.com>
2020-10-27 00:51:35 +0300

    added tests

--- awwardapp/tests.py
+++ awwardapp/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import post
+from users.models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.user = User(username='Mornica')
+        self.user.save()
+        self.post_test = post(id=1,caption='Denim wear', image='default.jpg', title='fashion')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post_test, post))
+
+    def test_save_post(self):
+        posts = post.objects.all()
+
+    def test_delete_post(self):
+        before = Profile.objects.all()
+        after = Profile.objects.all()
+        self.assertTrue(len(before) == len(after))
+
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+from django.test import TestCase
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='Mornica')
+        self.user.save()
+
+        self.profile_test = Profile(user=self.user, image='default.jpg' )
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    
+    
