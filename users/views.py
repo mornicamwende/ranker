98969fb79c0d064f63741e42d935984c1e999225
Mornica Mwende <mornicamwende@gmail.com>
2020-10-27 19:53:58 +0300

    add context

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import Profile
 from django.contrib.auth.decorators import login_required
 from .forms import  UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
@@ -9,7 +10,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
                 form.save()
-                profile = UserProfile.objects.create(user=request.user)
+                # profile = UserProfile.objects.create(user=request.user)
                 username = form.cleaned_data.get('username')
                 messages.success(request, f' Your account has been created! You are now able to log in!')
                 return redirect('login')
@@ -20,16 +21,13 @@ def register(request):
 @login_required
 def profile(request):
     if request.method =='POST':
-        
-        u_form=UserUpdateForm(request.POST,instance=request.user)
+        u_form=UserUpdateForm(request.POST, instance=request.user)
         p_form=ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
             messages.success(request, f' Your account has been updated!')
             return redirect('profile')
-
     else:
         u_form=UserUpdateForm(instance=request.user)
         p_form=ProfileUpdateForm(instance=request.user.profile)
