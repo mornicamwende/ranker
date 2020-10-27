107bcaad61bb5e726570a8250b55eb2e6245dc7a
Mornica Mwende <mornicamwende@gmail.com>
2020-10-27 20:07:21 +0300

    add requirements.txt

---
+++ awwardapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, home
+from .import views
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('', PostListView.as_view(), name='Awward-Home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    
+]
